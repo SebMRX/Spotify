39daf24d4bfafad50b11ddee88bf38ae58fade2a
MRX <142695072+SebMRX@users.noreply.github.com>
2025-02-20 21:09:24 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,14 +7,14 @@ spotify_url = "https://api.spotify.com/v1/users/YOUR_ID/playlists"
 id = "YOUR_ID"
 secret = "YOUR_SECRET_KEY"
 
-# Scraping Billboard 100 Web Site
+
 date = input("Which year do you want to travel to? Type the date in this format YYYY-MM-DD: ")
 response = requests.get("https://www.billboard.com/charts/hot-100/" + date)
 soup = BeautifulSoup(response.text, 'html.parser')
 song_names_spans = soup.select("li ul li h3")
 song_names = [song.getText().strip() for song in song_names_spans]
 
-#Spotify Authentication with spotipy
+
 sp = spotipy.Spotify(
     auth_manager=SpotifyOAuth(
         scope="playlist-modify-private",
@@ -29,7 +29,7 @@ sp = spotipy.Spotify(
 user_id = sp.current_user()["id"]
 print(user_id)
 
-#Searching Spotify for songs by title
+
 song_uris = []
 year = date.split("-")[0]
 for song in song_names:
@@ -41,7 +41,7 @@ for song in song_names:
     except IndexError:
         print(f"{song} doesn't exist in Spotify. Skipped.")
 
-#Creating a new private playlist in Spotify. If you want to make public, change the public parameter.
+
 playlist = sp.user_playlist_create(user=user_id, name=f"{date} BEST 100 MUSIC", public=False)
 print(playlist)
 
